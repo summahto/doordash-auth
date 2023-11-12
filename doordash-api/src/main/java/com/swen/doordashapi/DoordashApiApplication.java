e25e70138f589cd9785dff22e8794d09ecbb0175
summahto <sumitkumarmahto3@gmail.com>
2023-11-12 16:24:02 -0500

    backend

---
+++ doordash-api/src/main/java/com/swen/doordashapi/DoordashApiApplication.java
@@ -0,0 +1,13 @@
+package com.swen.doordashapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoordashApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoordashApiApplication.class, args);
+	}
+
+}
---
+++ doordash-api/src/test/java/com/swen/doordashapi/DoordashApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swen.doordashapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoordashApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
